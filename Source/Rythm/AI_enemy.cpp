aade7ad4824d50e075005452541e0a3713934891
perfest <perfect2111@gmail.com>
2020-12-29 01:55:08 +0300

    conflicts

---
+++ Source/Rythm/AI_enemy.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "Main_Hero.h"
+#include "Enemy.h"
+#include "AI_enemy.h"
+
+
+void AAI_enemy::Tick(float DeltaTime)
+{
+	if(PawnAsEnemy)
+	{
+		if(AActor* Target=PawnAsEnemy->GetTarget())
+		{
+			// We do have a target. Shamble toward it and attempt violence! У нас есть цель. Тащитесь к нему и пытайтесь насилие!
+			FVector DirectionToTarget = (Target->GetActorLocation() - PawnAsEnemy->GetActorLocation()).GetSafeNormal2D();
+			float DotToTarget = FVector::DotProduct(DirectionToTarget, PawnAsEnemy->GetActorForwardVector());
+			float SidewaysDotToTarget = FVector::DotProduct(DirectionToTarget, PawnAsEnemy->GetActorRightVector());
+			float DeltaYawDesired = FMath::Atan2(SidewaysDotToTarget, DotToTarget);
+		}
+	}
+}
+
+void AAI_enemy::OnPossess(APawn* InPawn)
+{
+	Super::Possess(InPawn);
+	PawnAsEnemy = Cast<AEnemy>(GetPawn());
+}
+
+void AAI_enemy::OnUnPossess()
+{
+	Super::UnPossess();
+	PawnAsEnemy = nullptr;
+}
